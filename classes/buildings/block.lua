901fa04913d75c47d237301e265b7e9c21475c16
a0405u <a0405u@ya.ru>
2025-08-06 02:33:18 +0300

    Feature: Block sticking

--- classes/buildings/block.lua
+++ classes/buildings/block.lua
@@ -23,15 +23,6 @@ function BuildingBlock:init(position, rotation, sprite, dl)
     self.connected = {}
     self.nails = {}
     self.joints = {}
-    self.sensors = {
-        love.physics.newFixture(self.body, love.physics.newRectangleShape(DIR[1].x * TILESIZE, DIR[1].y * TILESIZE, SNS_BLOCK, SNS_BLOCK), DS_SENSOR),
-        love.physics.newFixture(self.body, love.physics.newRectangleShape(DIR[2].x * TILESIZE, DIR[2].y * TILESIZE, SNS_BLOCK, SNS_BLOCK), DS_SENSOR),
-        love.physics.newFixture(self.body, love.physics.newRectangleShape(DIR[3].x * TILESIZE, DIR[3].y * TILESIZE, SNS_BLOCK, SNS_BLOCK), DS_SENSOR),
-        love.physics.newFixture(self.body, love.physics.newRectangleShape(DIR[4].x * TILESIZE, DIR[4].y * TILESIZE, SNS_BLOCK, SNS_BLOCK), DS_SENSOR),
-    }
-    for i, sensor in ipairs(self.sensors) do
-        sensor:setSensor(true)
-    end
     self.contacts = {}
 end
 
@@ -46,8 +37,8 @@ function BuildingBlock:update(dt)
     Building.update(self, dt)
 
     for key, contact in pairs(self.contacts) do
-        if contact.sensor:testPoint(contact.block.body:getPosition()) then
-            self:connect(contact.block, contact.direction)
+        if contact.object:is(BuildingBlock) and contact.secondary then
+            self:connect(contact.object)
             self.contacts[key] = nil
         end
     end
@@ -62,57 +53,49 @@ function BuildingBlock:destroy(position)
     Building.destroy(self, position)
 end
 
---- Connects block to this block at given side
+--- Connect block to self
 --- @param block BuildingBlock block to connect
---- @param direction Vector normal from the center pointing to the side
-function BuildingBlock:connect(block, direction)
+function BuildingBlock:connect(block)
 
-    for i, b in ipairs(self.connected) do
-        if b == block then return end
-    end
+    if self.connected[block] then return end
 
-    local rotation = self.body:getAngle()
-    local position = self.position + direction:rotated(rotation) * TILESIZE
-
-    block.body:setAngle(rotation)
-    block.body:setPosition(position:split())
-
-    local x = (self.position.x + position.x) / 2.0
-    local y = (self.position.y + position.y) / 2.0
+    local x = (self.position.x + block.position.x) / 2.0
+    local y = (self.position.y + block.position.y) / 2.0
     local joint = love.physics.newWeldJoint(self.body, block.body, x, y)
 
     table.insert(self.joints, joint)
     table.insert(block.joints, joint)
-    table.insert(self.connected, block)
-    table.insert(block.connected, self)
+
+    self.connected[block] = block
+    block.connected[self] = self
 end
 
 
 function BuildingBlock:begincontact(a, b, contact)
 
-    local body = b:getBody()
-    local object = body:getUserData()
+    local object = b:getBody():getUserData()
+    local xa, ya, xb, yb = contact:getPositions();
+    local cache = {
+        position = Vector(xa, ya),
+        secondary = xb and Vector(xb, yb) or nil, -- might be absent
+        normal = Vector(contact:getNormal()),
+        object = object
+    }
 
-    if object:is(BuildingBlock) then
-        for i, sensor in ipairs(self.sensors) do
-            if a == sensor then self.contacts[object] = {block = object, sensor = a, direction = DIR[i]} end
-        end
-    end
+    self.contacts[object] = cache
 end
 
 
 function BuildingBlock:endcontact(a, b, contact)
 
-    local body = b:getBody()
-    local object = body:getUserData()
+    local object = b:getBody():getUserData()
 
-    if object:is(BuildingBlock) then
-        self.contacts[object] = nil
-    end
+    self.contacts[object] = nil
 end
 
 
 -- function BuildingBlock:postsolve(a, b, contact, normalimpulse, tangentimpulse)
+    -- Building.postsolve(self, a, b, contact, normalimpulse, tangentimpulse)
 
     -- audio.play(sound.collide)
     -- if normalimpulse > 1000 then print(normalimpulse) end
